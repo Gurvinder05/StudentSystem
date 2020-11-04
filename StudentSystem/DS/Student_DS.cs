8539d9e655c976b93e6d29d9c0974c5d4122fb39
Arshdeep Singh <AS131@atcltd.atc.org.nz>
2020-11-04 21:44:24 +1300

    Add project files.

---
+++ StudentSystem/DS/Student_DS.cs
@@ -0,0 +1,16 @@
+﻿namespace StudentSystem.DS
+{
+
+
+    partial class Student_DS
+    {
+       
+    }
+}
+
+namespace StudentSystem.DS.Student_DSTableAdapters {
+    
+    
+    public partial class USP_STUDENT_SEARCH_BY_TEACHERTableAdapter {
+    }
+}
